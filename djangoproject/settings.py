b91bda5e0af4f46cfa7155c7d85b72cc4348a527
Emily  Murphy <Emily.Murphy@emurphy.2ldxhoqz4w0efi5ajh3u0k0m2g.bx.internal.cloudapp.net>
2018-10-29 19:34:37 +0000

    Add features and CSS

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,6 +99,10 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+STATICFILES_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'static').replace('\\','/'),
+)
 
 
 # Internationalization
